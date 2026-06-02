ae66a07896b15535e0ae0a64560c1c44614074f4
raim <rainer.machne@gmail.com>
2026-06-02 13:53:00 +0200

    added glottolot language tree and educational tree for current languages

--- tree/tree.R
+++ tree/tree.R
@@ -1,16 +1,16 @@
 library(ape)
 
 ## full glottolot tree
-flt <- ape::read.tree("/home/raim/programs/dreizunge/literature/tree_glottolog_newick.txt")
+flt <- ape::read.tree("tree/tree_glottolog_newick.txt")
 
 ## TODO: load languages and filter glottolog tree, to get at least
 ## all branches right
 ##plot(flt, dir='downwards')
 
 ## reduced tree
-lt <- ape::read.tree("/home/raim/programs/dreizunge/app/plans/langs_newick.txt")
+lt <- ape::read.tree("tree/langs_newick.txt")
 
-svg(file="/home/raim/programs/dreizunge/app/plans/langs_newick.svg")
+svg(file="tree/langs_newick.svg")
 plot(lt, dir='downwards')
 nodelabels(
   text = lt$node.label,
